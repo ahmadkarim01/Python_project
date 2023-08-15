713c0f564c75917c8280b48ba44d41af6033867f
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-15 20:24:05 +0500

    EmoWeek6.py

---
+++ EmoWeek6.py
@@ -0,0 +1,66 @@
+#funtions:
+"""A function is a block of code which only runs when it is called.
+
+You can pass data, known as parameters, into a function.
+
+A function can return data as a result."""
+
+
+
+# def my_function():
+#     print("I am calling to the function")
+# my_function()
+
+
+# def my_function(fname):
+#     print(fname +" Karim")
+
+# my_function("Ahmad")
+# my_function("Shahid")
+# my_function("Bena")
+
+
+#This function expects two arguments and get two arguments:
+# def my_data(fname,lname):
+#     print(fname +" "+ lname)
+
+# my_data("Ahmad","Karim")
+
+
+#Arbitrary Arguments, *args
+# def my_function(*students):
+#     print(students[1] + " \nis the largest boy in our class" )
+
+# my_function("Arslan","Kabir","Sadiq","Aslam")
+
+
+# Keyword Argumants:
+# def my_function(boy3, boy2, boy1):
+#     print("The youngest boy is " + boy3)
+
+# my_function(boy1 = "Ahmad", boy2 = "Taju", boy3 = "Yousuf")
+
+# Defualt parameter value:
+#The following example shows how to use a default parameter value.
+#If we call the function without argument, it uses the default value:
+# def my_function(country = "Pakistan"):
+#     print("I am from " + country)
+
+# my_function("India")
+# my_function("Indonisia")
+# my_function()
+# my_function("China")
+
+# def describe_city(city = "Gilgit"):
+#     print(city + " is in Pakistan")
+# describe_city()
+
+# def describe_city(city = "Islamabad"):
+#     print(city + " is my city ")
+# describe_city("Dehli")
+# describe_city("Islamabad")
+# describe_city("Norway")
+
+
+
+
