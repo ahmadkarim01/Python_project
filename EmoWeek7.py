0693eb49d0f9394160bc9fcf854bdd795490cc3c
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-15 20:42:55 +0500

    EmoWeek4..py  EmoWeek7.py  EmoWeek8.py

---
+++ EmoWeek7.py
@@ -0,0 +1,37 @@
+#Table
+# def table(num):
+#     for i in range(1,11):
+#         print(num, '*', i,'=',num*i)
+# input = int(input("Enter the number to print table\n"))
+# table(input)
+
+#Returning function:
+# def returning(list1):
+#     myName = list1
+#     return myName
+# name = returning(["Ahmad",'Saqib'])
+# print(name)
+
+
+# HOME_TASK
+# Create a function which will return list of all the data types:
+# def returning(mylist):
+#     Name = mylist
+#     return Name
+# list = returning(["hiy",3,34.3,'erdf'])
+# print(list)
+
+
+# dict = {"name":"Ahmad", "Roll No":"2343"}
+# print(dict)
+
+
+
+x = 0
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+else :
+    print('LARGE')
+print('All done')
---
+++ EmoWeek8.py
@@ -0,0 +1,19 @@
+
+# class fruit:
+#     def __init__(self, name, color, size, taste):
+#         self.name = apple,
+#         self.color = red,
+#         self.size = circle,
+#         self.taste = savory,
+#     print(name)
+
+
+class fruit:
+  def __init__(self, name, color):
+    self.name = name
+    self.color = color
+   
+    
+name = fruit("Apple","red")
+print("The name of the fruit is: ",name.name)
+# print(color.red)
