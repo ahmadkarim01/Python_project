c04d87f73efd8a379f0cb301f773a47b1a69e085
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-18 19:52:26 +0500

    coderr

---
+++ coderr/EmoDay3.py
@@ -0,0 +1,38 @@
+# Week 3.. Quix 1. Assignment
+''' Q.1Write program which will take all side of the rectangle one by one and print the area of the
+rectangle.'''
+length = int(input("Enter the lenth of triangle:"))
+width = int(input("Enter the width of triangle:"))
+area = length * width
+print(area)
+
+# Q.2 Program which will take 5 float points and give average of all inputs.
+num1 = float(input("Enter the first num"))
+num2 = float(input("Enter the second num"))
+num3 = float(input("Enter the third num"))k
+num4 = float(input("Enter the forth num"))
+num5 = float(input("Enter the fifth num"))
+sum = (num1 + num2 + num3 + num4 + num5)
+avg = sum/5
+print(avg) 
+
+'''Q.3 Take three integers and print the product of the given integers and 4th cubic of the the given
+numbers'''
+num1 = int(input("Enter the first integer:"))
+num2 = int(input("Enter the second integer:"))
+num3 = int(input("Enter the third integer:"))
+product = (num1*num2*num3)
+print(product)
+cubic = product**3
+print(cubic)
+
+'''Q.4Write a program which will take two inputs and give out put in a same line'''
+a = str(input("what is your name? "))
+b = str(input("who is your friend? "))
+print(a+(" my friend is "+b))
+
+'''Q6:Take two inputs from user and check whether they are equal or not.'''
+
+
+
+
---
+++ coderr/emodian practice.py
@@ -0,0 +1,40 @@
+# """x = 15;
+x = x + 5;
+print(x)"""
+
+"""python = "good bye"
+print(python)
+
+x = "Hello, Python i want to learn your syntex"
+print(x)
+
+strOne = str("pynative")
+strTwo = "pynative"
+print(strOne==strTwo)
+print(strOne is strTwo)"""
+
+# a = "WELCOME"
+# print(a*5)
+
+
+
+str1 = "my isname isisis jameis isis bond "
+str1 += "hello"
+print(str1)
+# print(len(str1.split()))
+str1 = "My name is Jame Bond is is"
+# str1 += "hello"
+# print(str1)
+# print(len(str1.split()))
+
+str = "jhfj"
+print(str)
+
+
+
+
+
+
+
+
+
