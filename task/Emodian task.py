bc4de5054196649dd824a50b2e5003326a216421
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-23 21:15:54 +0500

    task

---
+++ task/Emodian task.py
@@ -0,0 +1,80 @@
+#Week2- Quiz 1
+from operator import truediv
+from re import X
+
+
+#ntrective mode and displaying the chevron prompt(>>>)- what question is python asking you?
+Ans: What python script would you like to me run?
+
+#Q2.What will the following program printout?
+ x = 15
+ x = x + 5;
+print x
+Ans: 20
+
+#Q3.Python value is enclosed with?
+Ans: It is enclosed with doubled qoutes("")
+
+#Q4.What are reserved words in python?
+Ans: The python language reserves a small set of keywords that designate special language functionality. 
+
+#Q.5 What is the proper way to say "good bye" to Python?
+Ans:The good way to good bye to python is as under:
+python = "good-bye"
+print("python")
+
+#Q6.Which of the parts of the computer actually executes the program instructions?
+Ans: The CPU is heart of the computer.A program is a sequence of instructions store into main memory. When the program is run , the CPU fatches the instructions and exeutes or follows the instructions.
+
+#Q7.What is "code" in the context of this course?
+Ans: The code provides facilities to implement read eval print loops.
+
+#Q8.What is the best way to think about a "Syntex Error" while programming?
+Ans: According to my opinion, the best way to overcome syntex error is to be correct the gramatical spelling of the code and to be focused on the code with a good way.x
+
+#Q9.Write a python code which give the following output:
+Hello, I want to learn your syntex.
+Note: The python and the syntex will store in the variables and that will be called by formate.
+Ans:   x = "Hello, Python i want to learn your syntex"
+print(x)
+
+#Q11.What will the putput of the following program?
+strOne = str("pynative")
+strTwo = "pynative"
+print(strOne==strTwo)
+print(strOne is strTwo) 
+Ans:True
+    True
+
+#Q12.Choose a method and write a program which will convert String "Welcome to the beautiful world of python"   
+str = "Welcome to the beautiful world of python"
+print(str)
+
+#Q13.Write a program which will print "welcome" five times.
+Ans: a = "WELCOME"
+print(a*5)
+
+#Q14.Correct the below program which will give output as 7.
+str1 = "my isname isisis jameis isis bond"
+_____ = "is"
+print(str1._____(sub))
+Ans:
+str1 = "My name is Jame Bond is is"
+str1 += "hello"
+print(str1)
+print(len(str1.split()))
+
+#Q15. Write a program which give the string"Emodian Tech" as the following output: "EmodianTech.com"
+SoftwareHouse = "EmodianTech.com"
+print(SoftwareHouse)
+
+#Q16. Check the data type of the following strings
+str1 = "My salary is 7000";
+	str2 = "7000"
+	give the output as:
+	False
+	True
+
+
+
+ 
