85a42865e288cf2dbf7dfb1c41d5e1b7c1a296dc
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-23 21:23:02 +0500

    main.py

---
+++ Mycourse/main.py
@@ -0,0 +1,28 @@
+# salary=int(input("salary = "))
+
+# if salary>1000 and salary < 10000:
+#     print("salry is in between 1000 and 10000")
+#     print(salary>1000 or salary < 10000)
+
+# elif salary>10000 and salary <40000:
+#     print("salary is in between 10000 and 40000")    
+
+# elif salary>40000 and salary < 90000:
+#     print("salary is in between 40000 and 90000")
+# else:
+#     print(" This catogory is not defined") 
+# 
+# print("hello " *100)  
+
+itm=[1,2,3,4,5]
+itm[0]
+for x in itm:
+    print(x)
+
+
+x=0
+while x<100:
+    print(x)
+    x +=1
+
+
