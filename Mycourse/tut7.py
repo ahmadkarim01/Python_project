54b6bc11a387b7298fb9e4151a4d3b353cb5a334
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-25 22:09:39 +0500

    tut7.py

---
+++ Mycourse/tut7.py
@@ -0,0 +1,57 @@
+# here i am going to show you some variables that how they are decleared in a programme.
+print("Name the software houses present in Gilgit Baltistan:")
+var1 = "Uconnect"
+var2 = "Stp"
+var3 = "Emodian Tech"
+var4 = "Techzoid"
+var5 = "Techscape"
+var6 = "Incubation center"
+'''print(var1,var2,var3)
+print(var4,var5,var6)'''
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+
+print("I am computer science student")
+print("Being a computer science student i need to be problem solver and crital  thinker.")
+
+var7 = "1/2 * 0.5"
+print(type(var1))
+print(type(var7))
+
+
+input in a program.
+print("Enter the number:")
+inpnum = input()
+print("You entered", int(inpnum))
+print("ahmad")
+
+print("What is your name?")
+inpname = input()
+
+print("What is your father name?")
+inpfname = input()
+
+print("What is your qualification?")
+inpqualification = input()
+
+print("Enter the number:")
+inpnum = input()
+print("You enter", int(inpnum) + 10)"""
+
+
+
+
+#Quize no.1:
+# Create a simple calculator which will add two numbers.
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+sum = a + b
+print(sum)
+
+
