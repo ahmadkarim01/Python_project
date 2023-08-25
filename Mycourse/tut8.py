282e22698d0628d19226885d391776e662464602
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-25 22:10:33 +0500

    tut8.py

---
+++ Mycourse/tut8.py
@@ -0,0 +1,16 @@
+# Here i will discuss about python strings.
+# """mystr = "Ahmad is a good programmer"
+# print(mystr)
+# print(type(mystr))
+# print(mystr.find("good"))
+# print(mystr.upper())
+# print(mystr.lower())"""
+# print("Is this Ahmad's PC?\nYes")   # This sign (\n) shows the next line.
+# print("i love python programming")
+
+print(3 * ('ahmad' + 'karim'))
+print('Py'  'thon')
+
+
+
+
