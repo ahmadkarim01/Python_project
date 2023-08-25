c1d1e0a257ea22f3a3ebabc5b16b01457f424c32
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-25 22:11:14 +0500

    tut9.py

---
+++ Mycourse/tut9.py
@@ -0,0 +1,17 @@
+#Strings in python.
+# students = ("ahmad", "nasir", "tajwar","yousuf")
+# print(students)
+# numbers = [1, 3, 4, 5, 6]
+# print(numbers)
+# # numbers.sort()
+# print(max(numbers))
+# print(min(numbers))
+# print(len(numbers))
+# numbers.reverse()
+# numbers.append(99)
+
+
+my_list = ["Ahmad", "13", 'H', "[Pakistan]"]
+print(my_list)
+
+
