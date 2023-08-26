7e34c67e1590b97ea840f0ad7221a00426e74129
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 00:04:30 +0500

    tut15.py

---
+++ Mycourse/tut15.py
@@ -0,0 +1,22 @@
+# Exercise 2. Faulty Calculator:
+# Design a calculator which will correctly 
+# solve all the problems except the following ones:
+# 45 * 3 = 555, 56 + 9 = 77, 56/6 = 4
+# Your program should take operator and the two numbers as input from 
+# the user and then return the result.
+
+# a = int(input("Enter the two numbers: \n"))
+
+# b = int(input())
+
+x = 56
+y = 9
+if x+y == 65:
+print(x+y)
+
+# if inpu
+#     print("Answer is false")  
+
+
+    
+
