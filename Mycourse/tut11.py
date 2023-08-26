c157e9fe9cede3e5bc61bdcbe1da84e03c966d00
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 00:01:52 +0500

    tut11.py

---
+++ Mycourse/tut11.py
@@ -0,0 +1,17 @@
+'''Exercise1: Create a dictionary and take input from the user ans return the meaning of the word from the dictionary.'''
+
+word1 = (input("Enter the first word:\n"))           #concern
+dict = {"somthing that interest you because it's important"}
+print(dict)
+
+word2 = (input("Enter the sencond word:\n"))         #issue
+dict = {"some situation that is thought about"}
+print(dict)
+
+word3 = (input("Enter the third word:\n"))          #Approach
+dict = {"Move towards"}
+print(dict)
+
+word4 = (input("Enter the forth word:\n"))         #establish
+dict = {"Setup or found"}
+print(dict)
