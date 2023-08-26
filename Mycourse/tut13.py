99da53e6900a88ce51a65cc4809e491983aaf47d
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 00:03:35 +0500

    tut13.py

---
+++ Mycourse/tut13.py
@@ -0,0 +1,47 @@
+#Quiz
+'''print("Enter your age:" )
+
+age = int(input())
+
+if age > 18:
+    print("You are allow to derive a car")
+
+else:
+    print("You are not allow to derive")
+
+
+
+
+    # Here i am going to use if else statement in python program.
+
+user = int(input("Commend:\n"))
+
+if user>=50:
+    print("You are safe and you can go through destination.")
+
+elif user==50:
+    print("You can not move onword!")
+
+else:
+    print("You are in danger! Please change your path!")
+    print("This path may be danger for you")'''
+
+
+# a = 348
+# b = 523
+# print("Enter the values of a and b:")
+
+# if a > b:
+#     print("A is greater than B")
+
+# elif a < b:
+#     print("A is less than B")
+
+a = ahmad
+b = asam
+
+if a > b:
+    print("a is greater than b")
+
+else:
+    print("a is not greater than b")
