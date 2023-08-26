f55cc7816005c1f90fab23ea4ac53d835adddca4
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 00:02:43 +0500

    tut12.py

---
+++ Mycourse/tut12.py
@@ -0,0 +1,8 @@
+'''Here I just gonna show you how to initialze sets
+=> sets are used to store multiple items in a single variable.'''
+thisset = {1,2,4,5,5}
+print('The given numbers are:',thisset)
+
+thisset.add("6")                            # here i add a value in a set.
+print('After I add a number the values are:', thisset)
+
