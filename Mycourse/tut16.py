89733904dcf3286b42e2090bf0010fd8aacd5dc5
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 21:42:31 +0500

    tut16.py

---
+++ Mycourse/tut16.py
@@ -0,0 +1,33 @@
+#python loops
+'''list1 = ["Ahmad", "Nasir", "Tajwar", "Waqas", "Ahmar"]
+list2 = ["2", "3", "84", "43"]
+
+for item in list1:
+    print(item)
+
+for item in list2:
+    print(item)'''
+
+# Quiz:
+# Write a program by using lists which will print greater than six.
+
+''''items = [int,3,4,5,6,8,9,10,11,34,43,64,67]
+
+for item in items:
+     if str(item).isnumeric() and item>=6:
+        print(item)'''
+
+# Break statemnt used in loop
+fruits = ["apple", "orange", "Grapes", "Stawbary"]
+
+for x in fruits:
+    if x == "Grapes":
+        break
+    print(x)
+
+
+
+
+
+
+
