357ce5aa517f6301a17785f5f2a485e54e9fd582
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-27 21:48:47 +0500

    tut22.py

---
+++ Mycourse/tut22.py
@@ -0,0 +1,50 @@
+# Short hand if else statement.
+# a = int(input("Enter a\n"))
+# b = int(input("Enter b\n"))
+
+# if a>b:
+#     print("A is greater than B")
+
+# elif a<b:
+#     print("A is smaller then B")
+
+# else:
+#     print("A is equal to B")
+
+
+
+
+# a = int(input("enter a\n"))
+# b = int(input("enter b\n"))
+
+# if a>b :
+#     print("A is greater than b")
+
+# else:
+#     print("B is greater then A")
+# print("I love programming")
+
+# a = 23
+# b = 39
+# print(a,b)
+
+
+# Here I am going to share you the marks of the student he/she is pass or fail.
+total_marks = int(input("Enter total marks: \n"))
+obtain_marks = int(input("Enter Obtain marks:\n"))
+parcentage = (obtain_marks/total_marks *100)
+
+if parcentage >= 80:
+    print("A+")
+if parcentage >=70 and parcentage <=79:
+    print("A")
+if parcentage >=60 and parcentage <=69:
+    print("B")
+if parcentage >=50 and parcentage <=59:
+    print("C")
+if parcentage >=40 and parcentage <=49:
+    print("D")
+else:
+    ("You are fail in this year")
+
+
