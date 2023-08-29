e765311f2e2fb36fafa97704a2cc4ce3c9050129
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-29 23:18:08 +0500

    'rendom project.py'

---
+++ Mycourse/rendom project.py
@@ -0,0 +1,33 @@
+# Suppose you are university student
+#  and you need pay $4535 fee for the
+#  next semester.The college is giving
+#  you a discount of 10% on the early
+#  payment of your tuition fee.Can you
+#  find how much money you have to pay?
+ 
+# fee = 4535
+
+# discount_percent = 10
+
+# discount_amount = (discount_percent / 100) * fee
+
+# discounted_fee = discount_amount
+
+# print("Fee after discount:", discounted_fee )
+
+
+
+# Quiz
+# Create a program to convert distance
+#  into kilometers to miles.
+# formula: 1km -> 0.6213
+
+# distance_km = 564.7
+
+#1km = 0.6213
+
+distance_km = 564.7
+
+miles = (distance_km * 564.7)
+
+print("The distance is:",miles)
