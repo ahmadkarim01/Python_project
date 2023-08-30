1f662053da5f0e16bdd2e972923780e201cdfa2d
Ahmad Karim <ahmadkarimarbab@gmail.com>
2023-08-30 22:47:50 +0500

    pro1.py

---
+++ Python.org docs practice/pro1.py
@@ -0,0 +1,19 @@
+# Hello everyone to python.org
+# if and else statment.
+
+# name = "Ahmad Karim"
+# reg no = "23423"
+# father name  = "Karim Murad"
+# subject = "Bachelors in computer science"
+# cnic = "3423123423423"
+# profession = "programmer"
+a = 9
+if a < 9:
+    print("The entered number is less then nine")
+
+elif a > 9:
+    print("The given number is greater than nine") 
+
+else:
+    print("The given number is equal to nine")
+  
